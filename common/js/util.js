cc35b45f87fe9b3962c00297295d8ee0330e45dc
whxmm0608 <1159460622@qq.com>
2019-08-22 16:03:12 +0800

    1

--- common/js/util.js
+++ common/js/util.js
@@ -1,3 +1,48 @@
+
+function getScrollOffset() {
+
+    if (window.pageXOffset) {
+        return {
+            x: window.pageXOffset,
+            y: window.pageYOffset
+        }
+    } else {
+        return {
+            x: document.body.scrollLeft + document.documentElement.scrollLeft,
+            y: document.body.scrollTop + document.documentElement.scrollTop
+        }
+    }
+}
+
+function getViewportOffset() {
+    if (window.innerWidth) {
+        return {
+            w: window.innerWidth,
+            h: window.innerHeight
+        }
+    } else {
+        if (document.compatMode === "BackCompat") {
+            //怪异模式，混杂模式
+            return {
+                w: document.body.clientWidth,
+                h: document.body.clientHeight
+            }
+        } else {
+            //标准模式
+            return {
+                w: document.documentElement.clientWidth,
+                h: document.documentElement.clientHeight
+            }
+        }
+    }
+}
+
+
+export {
+    getScrollOffset,
+    getViewportOffset
+}
+
 // toFixed兼容方法
 Number.prototype.toFixed = function(n) {
     if (n > 20 || n < 0) {
@@ -51,3 +96,4 @@ Number.prototype.toFixed = function(n) {
 //
 (8716.425).toFixed(2); //
 (-8716.425).toFixed(2); //负数前面记得加括号
+
