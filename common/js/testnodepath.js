8aa2e7a24ba20df6cb8c5ed1f3afa873702940ac
whxmm0608 <1159460622@qq.com>
2019-09-25 16:59:03 +0800

    ~

--- common/js/testnodepath.js
+++ common/js/testnodepath.js
@@ -4,3 +4,8 @@ path.resolve('/foo','/bar','/baz');
 console.log(path.resolve('/foo','/bar','/baz'));
 console.log(path.resolve(__dirname,'dist'));
 console.log(path.resolve(__dirname,'/dist'));
+
+let regex = /\.(png|jpe?g|gif|svg)(\?.*)?$/;
+console.log(regex.test('a.png'));
+console.log(regex.test('a.png.jpg'));
+console.log(regex.test('a.png?query=123'));//true 这种适用于网络图片
