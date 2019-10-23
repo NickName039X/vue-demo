167097d3dfeb7163f2bdc655a5c14e55ddf7aae9
whxmm0608 <1159460622@qq.com>
2019-10-23 13:51:29 +0800

    ~

---
+++ src/common/js/testlodash.js
@@ -0,0 +1,14 @@
+let _ = require('lodash');
+function debounce(){
+    if(timer){
+        clearTimeout(timer);
+    }
+    timer = setTimeout( function(){
+
+    });
+}
+
+console.log(_.camelCase('Foo Bar'));
+console.log(_.camelCase('--foo-bar--'));
+console.log(_.capitalize('FRED'));
+
