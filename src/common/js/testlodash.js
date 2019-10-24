3a3f89225a13b30a6852222ba9b3d6f864bb766e
whxmm0608 <1159460622@qq.com>
2019-10-24 10:57:07 +0800

    ~

--- src/common/js/testlodash.js
+++ src/common/js/testlodash.js
@@ -11,5 +11,8 @@ function debounce(){
 console.log(_.camelCase('Foo Bar'));
 console.log(_.camelCase('--foo-bar--'));
 console.log(_.capitalize('FRED'));
+console.log(_.camelCase('_A--foo-bar--ball'));
+console.log(_.camelCase('el-alert__closebtn'));
+
 
 _.throttle(debounce,1000,{trailing:false})
