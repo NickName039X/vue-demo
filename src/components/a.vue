4e0290b40ec20dab4e1a3b0a213f04656e868c91
v_jayfang <v_jayfang@digitalgd.com.cn>
2022-05-18 15:47:41 +0800

    调整目录解构

---
+++ src/components/a.vue
@@ -0,0 +1,32 @@
+$<template>
+    <div>
+        <b :top="top" />
+    </div>
+</template>
+<script>
+import b from "./b.vue";
+export default {
+    name: "",
+    components: { b },
+    props: [""],
+    data() {
+        return {
+            top: {
+                name: "fangsheng",
+                age: 18
+            }
+        };
+    },
+    watch: {},
+    created() {},
+    beforeMount() {},
+    mounted() {},
+    methods: {},
+    computed: {}
+};
+</script>
+<style lang="" scoped>
+* {
+    background: blanchedalmond;
+}
+</style>
---
+++ src/components/b.vue
@@ -0,0 +1,33 @@
+$<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+  export default {
+    name:'',
+    components: {},
+    props:{
+        top:{
+            type:Object,
+            default:()=>{}
+        }
+    },
+    data () {
+      return {
+
+      };
+    },
+    watch: {},
+    created() {},
+    beforeMount() {},
+    mounted() {},
+    methods: {},
+    computed: {}
+  }
+</script>
+<style lang='' scoped>
+*{
+    background: gray;
+}
+</style>
