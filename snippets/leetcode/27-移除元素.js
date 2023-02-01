de73fe95e5df3b1b0292c69e7e02ba02e043f475
v_jayfang <v_jayfang@digitalgd.com.cn>
2023-02-01 16:18:52 +0800

    刷题

---
+++ snippets/leetcode/27-移除元素.js
@@ -0,0 +1,9 @@
+var removeElement = function(nums, val) {
+    let slow = 0;
+    for(let fast = 0; fast < nums.length; fast++){
+        if(nums[fast] !== val){
+            nums[slow++] = nums[fast];
+        }
+    } 
+    return slow
+};
